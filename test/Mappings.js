7da9eb59fa524f0fa30a6780abfbc3447d82f08b
supercodernoshadow <dadebonojo@icloud.com>
2023-09-24 12:16:33 -0600

    mappings

--- test/Mappings.js
+++ test/Mappings.js
@@ -35,7 +35,10 @@ describe('Mappings', () => {
       expect(result[0]).to.equal('A Tale of Two Cities')
       expect(result[1]).to.equal('Charles Dickens')
 
-      // Homework: check book 2
+      result =  await contract.books(2)
+      expect(result[0]).to.equal('Les Miserables')
+      expect(result[1]).to.equal('Victor Hugo')
+
 
       let user1 = '0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB'
       let dai = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
